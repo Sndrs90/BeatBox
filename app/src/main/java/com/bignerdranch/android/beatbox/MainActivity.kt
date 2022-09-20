36a918aee5da77783371ab9b72d77cc9ad10d522
Sanders <sndrs90@gmail.com>
2022-09-20 10:19:17 +0300

    Connecting assets to use chapter

--- app/src/main/java/com/bignerdranch/android/beatbox/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/beatbox/MainActivity.kt
@@ -17,14 +17,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         beatBox = BeatBox(assets)
-        beatBox.loadSounds()
 
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.recyclerView.apply {
             layoutManager = GridLayoutManager(context, 3)
-            adapter = SoundAdapter()
+            adapter = SoundAdapter(beatBox.sounds)
         }
     }
 
@@ -32,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         RecyclerView.ViewHolder(binding.root) {
     }
 
-    private inner class SoundAdapter() :
+    private inner class SoundAdapter(private val sounds: List<Sound>) :
         RecyclerView.Adapter<SoundHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
                 SoundHolder {
@@ -46,6 +45,6 @@ class MainActivity : AppCompatActivity() {
         }
         override fun onBindViewHolder(holder: SoundHolder, position: Int) {
         }
-        override fun getItemCount() = 0
+        override fun getItemCount() = sounds.size
     }
 }
